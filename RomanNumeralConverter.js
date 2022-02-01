d91dce9d8bd7deba2cbedba936900242c371889b
Subrat Kumar Das <72245773+subratsir@users.noreply.github.com>
2022-02-01 17:54:36 +0530

    Create RomanNumeralConverter.js

---
+++ RomanNumeralConverter.js
@@ -0,0 +1,17 @@
+// Convert the given number into a roman numeral.
+// All roman numerals answers should be provided in upper-case.
+function convertToRoman(num) {
+    if (isNaN(num))
+        return NaN;
+    var digits = String(+num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
+}
+
+convertToRoman(36);
