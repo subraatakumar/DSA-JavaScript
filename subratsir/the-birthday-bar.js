03ea39f339e8fbbd03a037fe9b43a720b5421af7
Subrat Kumar Das <72245773+subratsir@users.noreply.github.com>
2022-02-28 17:57:00 +0530

    Create the-birthday-bar.js

---
+++ subratsir/the-birthday-bar.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+function birthday(s, d, m) {
+    // Write your code here
+    let result = 0;
+    for(let i = 0; i < s.length - (m - 1); i++) {
+        if (s.slice(i, i + m).reduce((a, b) => a + b) === d) {
+            result++;
+        }
+    }
+    return result;    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
